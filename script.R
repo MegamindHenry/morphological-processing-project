6a5c86dc42c377e5e366f7442cf91c529b79700b
Mentha7 <jingwen.li@student.uni-tuebingen.de>
2019-08-10 01:11:08 +0200

    update

--- script.R
+++ script.R
@@ -38,12 +38,75 @@ one_new <- union(one_clean[one_clean$Cha != overlap,], sg)
 distinct(one_new)
 dict_new <- union(one_new, two_clean)  # 5211 characters!
 save(dict_new, file = 'dict_new.Rda')
-save(dict_new, file = 'dict_new.csv')
+write.csv(dict_new, file = 'data/dict_new.csv', fileEncoding='UTF-8', row.names = FALSE, quote = FALSE)
 
 
-Sbig <- load('data/Sbig.rda')
+Sbig <- load('Sbig.rda')
 names <- rownames(Sbig) # set of 4708 characters in semantic matrix
 # length(unique(names))==length(names)
+
+
+
+wlist = c2p$char
+flist = c2p$pic
+cues = get_graphic_cues(allwords=wlist,
+                        fileNames=flist,
+                        folder='data/files_char',
+                        ext=".png")
+
+write.csv(cues,'data/chars_hog.csv')
+
+rad_po <- read.csv('data/rad_po_data.csv')
+ch_rad_po = read.csv('data/chars_rad_po_matrix.csv', header = TRUE, row.names = 1)
+dim(ch_rad_po)
+
+ch_hog = read.csv('data/chars_hog_matrix.csv', header = TRUE, row.names = 1)
+dim(ch_hog)
+
+load('data/Sbig.rda')
+dim(Sbig)
+
+write.csv(Sbig,'data/chars_s.csv')
+
+
+ch_rad_po_temp <- tibble::rownames_to_column(ch_rad_po, "Cha")
+
+ch_hog_temp <- tibble::rownames_to_column(ch_hog, "Cha")
+
+Sbig_temp <- tibble::rownames_to_column(as.data.frame(Sbig), "Cha")
+
+Cha_common = Reduce(intersect, list(ch_rad_po_temp$Cha, ch_hog_temp$Cha,Sbig_temp$Cha))
+
+ch_rad_po_clean_temp <- ch_rad_po_temp %>%
+  filter(Cha %in% Cha_common) %>%
+  arrange(Cha)
+ch_rad_po_clean_temp[1:30,1:3]
+ch_rad_po_clean <- ch_rad_po_clean_temp[,-1]
+
+ch_hog_clean_temp <- ch_hog_temp %>%
+  filter(Cha %in% Cha_common) %>%
+  arrange(Cha)
+ch_hog_clean_temp[1:30, 1:3]
+ch_hog_clean <- ch_hog_clean_temp[,-1]
+
+Sbig_clean_temp <- Sbig_temp %>%
+  filter(Cha %in% Cha_common) %>%
+  arrange(Cha)
+Sbig_clean_temp[1:30, 1:3]
+Sbig_clean <- Sbig_clean_temp[,-1]
+
+ch_hog_clean_matrix <- as.matrix(ch_hog_clean)
+ch_rad_po_clean_matrix <- as.matrix(ch_rad_po_clean)
+Sbig_clean_matrix <- as.matrix(Sbig_clean)
+
+save(ch_hog_clean_matrix, file = 'ch_hog_clean_matrix.Rda')
+save(ch_rad_po_clean_matrix, file = 'ch_rad_po_clean_matrix.Rda')
+save(Sbig_clean_matrix, file = 'Sbig_clean_matrix.Rda')
+write.table(ch_hog_clean_matrix, file = 'hog_matrix.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
+write.table(ch_rad_po_clean_matrix, file = 'rad_matrix.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
+write.table(Sbig_clean_matrix, file = 'Sbig_matrix.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
+
+Hcomp = ch_hog_clean_matrix %*% ginv(t(ch_hog_clean_matrix)%*%ch_hog_clean_matrix)%*%t(ch_hog_clean_matrix)
 
 
 
