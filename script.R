c76d4a1cea4dbb3db64aa19d4329437ed334b1bd
Mentha7 <jingwen.li@student.uni-tuebingen.de>
2019-08-15 23:03:24 +0200

    update

--- script.R
+++ script.R
@@ -14,21 +14,24 @@ library('MASS')
 
 # character-bitmap filename mapping
 c2p = read_csv('data/chars_pic.csv')
-head(c2p)
-str(c2p)
-# chars <- c2p$char # 4895 chars
+wlist = c2p$char
+flist = c2p$pic
+cues = get_graphic_cues(allwords=wlist,
+                        fileNames=flist,
+                        folder='data/files_char',
+                        ext=".png")
+write.csv(cues,file = 'data/chars_hog.csv')
 
 dict = read_csv('data/dict.csv', col_names = TRUE)
 unique(dict$Str) # 6 unique structures 
-# dchar <- dict$Cha  # 5807 characters
 one <- dict[is.na(dict$Com2),] # 408 characters with 1 component
 one_problematic <- one[one$Str != 'SG',]  # with 1 component but not SG structure
 one_clean <- setdiff(one, one_problematic)
 two <- dict[!is.na(dict$Com2) & is.na(dict$Com3),]
 two_problematic <- two[two$Str == 'SG',]  # with 2 components but SG structure
 two_clean <- setdiff(two, two_problematic) # 2 components only AND not SG structure
-twochar <- two_clean$Cha # 4886 characters with 2 components only and not SG structure
-more <- dict[!is.na(dict$Com3),]  # 461
+twochar <- two_clean$Cha 
+more <- dict[!is.na(dict$Com3),]
 
 sg <- read_csv('data/sg.csv', col_names = TRUE, col_types = cols('SeR'=col_character(),
                                                               'PhR' = col_character(),
@@ -39,31 +42,16 @@ sg <- read_csv('data/sg.csv', col_names = TRUE, col_types = cols('SeR'=col_chara
 overlap <- intersect(one_clean$Cha, sg$Cha)
 one_new <- union(one_clean[one_clean$Cha != overlap,], sg)
 distinct(one_new)
-dict_new <- union(one_new, two_clean)  # 5211 characters!
+dict_new <- union(one_new, two_clean)
 save(dict_new, file = 'dict_new.Rda')
 write.csv(dict_new, file = 'data/dict_new.csv', fileEncoding='UTF-8', row.names = FALSE, quote = FALSE)
 dict = read_csv('data/dict_new.csv', col_names = TRUE)
 
-wlist = c2p$char
-flist = c2p$pic
-cues = get_graphic_cues(allwords=wlist,
-                        fileNames=flist,
-                        folder='data/files_char',
-                        ext=".png")
-write.csv(cues,file = 'data/chars_hog.csv')
-
 rad_po <- read.csv('data/rad_po_data.csv')
 ch_rad_po = read.csv('data/chars_rad_po_matrix.csv', header = TRUE, row.names = 1)
-dim(ch_rad_po)
-
 ch_hog = read.csv('data/chars_hog_matrix.csv', header = TRUE, row.names = 1)
-dim(ch_hog)
 
 load('data/Sbig.rda')
-dim(Sbig)
-
-write.csv(Sbig,'data/chars_s.csv')
-
 
 ch_rad_po_temp <- tibble::rownames_to_column(ch_rad_po, "Cha")
 
@@ -76,19 +64,16 @@ Cha_common = Reduce(intersect, list(ch_rad_po_temp$Cha, ch_hog_temp$Cha,Sbig_tem
 ch_rad_po_clean_temp <- ch_rad_po_temp %>%
   filter(Cha %in% Cha_common) %>%
   arrange(Cha)
-  ch_rad_po_clean_temp[1:30,1:3]
 ch_rad_po_clean <- ch_rad_po_clean_temp[,-1]
 
 ch_hog_clean_temp <- ch_hog_temp %>%
   filter(Cha %in% Cha_common) %>%
   arrange(Cha)
-ch_hog_clean_temp[1:30, 1:3]
 ch_hog_clean <- ch_hog_clean_temp[,-1]
 
 Sbig_clean_temp <- Sbig_temp %>%
   filter(Cha %in% Cha_common) %>%
   arrange(Cha)
-Sbig_clean_temp[1:30, 1:3]
 Sbig_clean <- Sbig_clean_temp[,-1]
 
 ch_hog_clean_matrix <- as.matrix(ch_hog_clean)
@@ -102,17 +87,13 @@ Sbig_clean_matrix_removed <- Sbig_clean_matrix[-c(1314, 3670),]
 save(ch_hog_clean_matrix, file = 'ch_hog_clean_matrix.Rda')
 save(ch_rad_po_clean_matrix, file = 'ch_rad_po_clean_matrix.Rda')
 save(Sbig_clean_matrix, file = 'Sbig_clean_matrix.Rda')
+
 write.table(ch_hog_clean_matrix, file = 'hog_matrix.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
 write.table(ch_rad_po_clean_matrix, file = 'rad_matrix.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
 write.table(Sbig_clean_matrix, file = 'Sbig_matrix.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
 
-
-
 write.table(ch_hog_clean_matrix_removed, file = 'hog_matrix_removed.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
 write.table(ch_rad_po_clean_matrix_removed, file = 'rad_matrix_removed.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
 write.table(Sbig_clean_matrix_removed, file = 'Sbig_matrix_removed.csv', quote = FALSE, sep = ",", row.names = FALSE, col.names = FALSE, fileEncoding = "UTF-8")
-Hcomp = ch_hog_clean_matrix %*% ginv(t(ch_hog_clean_matrix)%*%ch_hog_clean_matrix)%*%t(ch_hog_clean_matrix)
-
-View(Cha_common)
 
 
